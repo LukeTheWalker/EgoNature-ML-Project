f80ae8e4f7a0d72f2c7f2c71eedd752ceb26f639
LukeTheWalker <lucag2001@gmail.com>
2023-06-17 10:27:20 +0200

    feat: added demos to run all_scripts

--- run_all_scripts.py
+++ run_all_scripts.py
@@ -4,6 +4,7 @@
 #
 
 import os
+import shutil
 import subprocess
 
 # get all the models
@@ -32,4 +33,26 @@ for model in models:
 for model in models:
     for modality in modalities:
         subprocess.run(["python", "metrics.py", "-m", "best_models", "-mod", modality])
+
+# run the 4 demos for each modality
+
+# first demo: py demo.py -m best_models/resnet18_ConSub_fold2.ckpt -i data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_02_21_000_0000059.jpeg -mod ConSub
+
+subprocess.run(["python", "demo.py", "-m", "best_models/resnet18_ConSub_fold2.ckpt", "-i", "data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_02_21_000_0000059.jpeg", "-mod", "ConSub"])
+shutil.move("results/result.png", "results/results_good1_2018_02_21_000_0000059.png")
+
+# second demo: py demo.py -m best_models/resnet18_Sub_fold1.ckpt -i data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_03_01_000_0006167.jpeg -mod Sub
+
+subprocess.run(["python", "demo.py", "-m", "best_models/resnet18_Sub_fold1.ckpt", "-i", "data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_03_01_000_0006167.jpeg", "-mod", "Sub"])
+shutil.move("results/result.png", "results/results_good2_2018_03_01_000_0006167.png")
+
+# third demo: py demo.py -m best_models/resnet18_Con_fold1.ckpt -i data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_03_01_000_0005496.jpeg -mod Con
+
+subprocess.run(["python", "demo.py", "-m", "best_models/resnet18_Con_fold1.ckpt", "-i", "data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_03_01_000_0005496.jpeg", "-mod", "Con"])
+shutil.move("results/result.png", "results/results_bad1_2018_03_01_000_0005496.png")
+
+# fourth demo: py demo.py -m best_models/resnet18_ConSub_fold2.ckpt -i data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_02_21_000_0000011.jpeg -mod ConSub 
+
+subprocess.run(["python", "demo.py", "-m", "best_models/resnet18_ConSub_fold2.ckpt", "-i", "data/EgoNature-Dataset/EgoNature-Dataset/EgoNature\ -\ CNN/2018_02_21_000_0000011.jpeg", "-mod", "ConSub"])
+shutil.move("results/result.png", "results/results_bad2_2018_02_21_000_0000011.png")
 
